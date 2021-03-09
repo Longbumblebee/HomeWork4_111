e177875d779e7cdac0aaf875cd58fe9e77612ade
Владислав Кузнецов <kz-vlad@mail.ru>
2021-03-09 19:53:10 +0300

    Добавьте файлы проекта.

---
+++ HomeWork4_111/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4_111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetFullName("Иван", "Иванов", "Иванович"));
+            Console.WriteLine(GetFullName("Петр", "Петров", "Петрович"));
+            Console.WriteLine(GetFullName("Сидор", "Сидоров", "Сидоврович"));
+            Console.ReadKey();
+        }
+
+        public static string GetFullName(string firstName, string lastName, string patronymic)
+        {
+            return lastName + " " + firstName + " " + patronymic;
+        }
+    }
+}
